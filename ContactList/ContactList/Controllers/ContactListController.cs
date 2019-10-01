b68ee806af4f6db94edc6d9a7705519470fd2b71
Daniel <20150107@students.htl-perg.ac.at>
2019-10-01 19:30:20 +0200

    add homework

---
+++ ContactList/ContactList/Controllers/ContactListController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ContactList.Controllers
+{
+    [ApiController]
+    [Route("contacts")]
+    public partial class ContactListController : ControllerBase
+    {
+        private static readonly List<Person> contacts =
+            new List<Person> ();
+
+        [HttpGet]
+        public IActionResult GetAllContacts() => Ok(contacts);
+
+        [HttpGet]
+        [Route("findByName", Name = "GetSpecificItem")]
+        public IActionResult GetContact([FromQuery] string nameFilter)
+        {
+            List<Person> filteredContacts = new List<Person>();
+            foreach (Person person in contacts)
+            {
+                if(person.firstName.ToLower().Contains(nameFilter.ToLower()) || person.lastName.ToLower().Contains(nameFilter.ToLower()))
+                {
+                    filteredContacts.Add(person);
+                }
+            }
+            if(filteredContacts.Count > 0)
+            {
+                return Ok(filteredContacts);
+            }
+
+            return BadRequest("Invalid or missing name");
+        }
+
+        [HttpPost]
+        public IActionResult AddContact([FromBody] Person newContact)
+        {
+            if(newContact.id == 0 || newContact.email == null)
+            {
+                return BadRequest("Invalid input (e.g. required field missing or empty)");
+            }
+            contacts.Add(newContact);
+            return Created("Person successfully created", newContact);
+        }
+
+        [HttpDelete]
+        [Route("{personId}")]
+        public IActionResult DeleteContact(int personId)
+        {
+            if(personId < 0)
+            {
+                return BadRequest("Invalid ID supplied");
+            }
+
+            foreach (Person person in contacts)
+            {
+                if (person.id == personId)
+                {
+                    contacts.Remove(person);
+                    return NoContent();
+                }
+            }
+
+            return NotFound("Person not found");
+        }
+    }
+}
---
+++ ContactList/ContactList/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class Person
+    {
+        public Int32 id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+    }
+}
